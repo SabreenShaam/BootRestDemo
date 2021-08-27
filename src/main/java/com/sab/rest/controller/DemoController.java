44cc9aa9d20828e20fb1caa28f350a474062b385
SabreenShaam <32370197+SabreenShaam@users.noreply.github.com>
2021-08-27 17:05:37 +0530

    url changed

--- src/main/java/com/sab/rest/controller/DemoController.java
+++ src/main/java/com/sab/rest/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @RequestMapping("/get")
+    @RequestMapping("/message")
     public  String  sample(){
         return "Sabreen Its Working";
     }
